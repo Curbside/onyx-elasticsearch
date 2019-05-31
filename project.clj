8ec1ed7339fdd6d987ce18e2920ab4fedfd9c3e1
Guillaume Carbonneau <guillaume.carbonneau@gmail.com>
2019-05-31 12:18:23 -0400

    Remove -alpha3+writer

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curbside/onyx-elasticsearch "0.13.0.0-alpha3+writer"
+(defproject curbside/onyx-elasticsearch "0.13.0.0"
   :description "Onyx plugin for Elasticsearch"
   :url "https://github.com/onyx-platform/onyx-elasticsearch"
   :license {:name "Eclipse Public License"
